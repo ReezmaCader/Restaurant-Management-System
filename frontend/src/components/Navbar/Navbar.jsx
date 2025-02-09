dfae741a3499ec439524f89967a4560b63717665
Reezma Cader <reezmacader2001@gmail.com>
2025-02-09 19:24:14 +0530

    Commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,41 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-    const [menu,setMenu] = useState("home");
+const Navbar = ({setShowLogin}) => {
+    const [menu, setMenu] = useState("home");
 
-  return (
-    <div className='navbar'>
-      <img src={assets.logo} alt="" className="log" />
-      <ul className="navbar-menu">
-      <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
-        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
-        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact us</li>
-      </ul>
-      <div className="navbar-right">
-        <img src={assets.search_icon} alt="" className="search"  />
-        <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" className="basket" />
-            <div className="dot"></div>
+    return (
+        <div className="navbar">
+            <Link to='/'><img src={assets.logo} alt="Logo" className="logo" /></Link>
+            <ul className="navbar-menu">
+                <li>
+                    <Link to="/" onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <a href="#explore-menu" onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>
+                        Menu
+                    </a>
+                </li>
+                <li>
+                    <a href="#footer" onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>
+                        Contact us
+                    </a>
+                </li>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="Search" className="search" />
+                <div className="navbar-search-icon">
+                    <Link to='/cart'><img src={assets.basket_icon} alt="Basket" className="basket" /></Link>
+                    <div className="dot"></div>
+                </div>
+                <button onClick={()=>setShowLogin(true)}>Sign in</button>
+            </div>
         </div>
-        <button>Sign in</button>
-      </div>
-     </div> 
-  )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
